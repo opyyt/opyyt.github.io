62d2d6c67133724ae2e4e88db4dfba29a80d5c1b
greed <opyyt@foxmail.com>
2022-09-17 21:33:08 +0800

    新增www.doublepdoublecat.xyz

---
+++ program/viteAtpige/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+  <div>
+    <div class="container">
+      <h1>Atpige</h1>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  padding: 0 200px 0 200px;
+}
+</style>
