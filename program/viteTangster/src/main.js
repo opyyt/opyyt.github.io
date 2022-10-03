a96a6bf6f1ef70d1234d6697268df7073381e9e1
greed <opyyt@foxmail.com>
2022-10-03 21:35:53 +0800

    新增个人作品项目

--- program/viteTangster/src/main.js
+++ program/viteTangster/src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
-
+import 'element-plus/dist/index.css'
+import './style.css'
 const app = createApp(App)
-app
-    .use(router)
-    .mount('#app')
+app.use(router)
+app.mount('#app')
---
+++ program/viteTangster/src/views/components/PublicWelfare.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>PublicWelfare</div>
+</template>
