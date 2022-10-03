2d0e7d025f71809ed1675325451361724103abfc
greed <opyyt@foxmail.com>
2022-10-03 18:54:29 +0800

    新增个人作品项目

---
+++ program/viteTangster/src/router/index.js
@@ -0,0 +1,20 @@
+import {
+    createRouter,
+    createWebHistory
+} from "vue-router";
+import home from '../views/Home.vue'
+
+const routes = [{
+    path: "/",
+    name: "Home",
+    component: home
+},];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+
+export default router;
+
